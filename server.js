48e104a1b2b4767c36cf1514bda4edc19d04165d
ali45452 <165812438+ali45452@users.noreply.github.com>
2025-03-29 21:33:36 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
+
+let locked = false;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  socket.on('buzz', (player) => {
+    if (!locked) {
+      locked = true;
+      io.emit('winner', player);
+      io.emit('lock');
+    }
+  });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+http.listen(process.env.PORT || 3000, () => {
+  console.log('Server running on port 3000');
+});
